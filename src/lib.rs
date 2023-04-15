847ae9675f4d8d9f2a50953a64ebf6dfbdd5eb67
williambilsdon <williambilsdon@gmail.com>
2023-04-15 21:16:40 +0100

    Added further tests for multiple items and started implementing custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use core::fmt;
+use std::fmt::Error;
+
 pub struct Bitreader<'a> {
     buffer: &'a [u8],
-    position: u8,
-    length: u8,
+    position: u64,
+    length: u64,
 }
 
 impl<'a> Bitreader<'a> {
@@ -9,43 +12,87 @@ impl<'a> Bitreader<'a> {
         Bitreader { 
             buffer: bytes, 
             position: 0, 
-            length: bytes.len() as u8 * 8
+            length: bytes.len() as u64 * 8
         }
     }
 
-    fn read_bits(&self, size: u8) -> u8 {
-        let mut result: u8 = 0;
-        let pos = self.position;
-        let end_pos = self.position + size;
+    fn read_bits(&mut self) -> Result {
+        let mut value: u8 = 0;
+        let start_pos = self.position;
+        let end_pos = start_pos + 8;
 
-        for i in pos..end_pos {
+        if (end_pos > self.length) {
+            BitReadError()
+        }
+
+        println!("start_pos : {}\nend_pos : {}", start_pos, end_pos);
+
+        for i in start_pos..end_pos{
             let index = (i / 8) as usize;
             let byte = self.buffer[index]; // 10
             let shift = 7 - (i % 8); // 0
             let bit = (byte >> shift) & 1; // 0001 0100 >> 0 = 0001 0100 & 1 = 0000 0000 
 
-            result = (result << 1) | bit; // 0000 1010 << 1 = 0001 0100 | 0000 0000  = 0001 0100 
-            println!("{}", result)
+            value = (value << 1) | bit; // 0000 1010 << 1 = 0001 0100 | 0000 0000  = 0001 0100 
         }
 
-        result
+        self.position = end_pos;
+
+        Ok(value)
     }
 
-    // pub fn read_string(&self, size: u8) -> [u8] {
-    //     let end_pos = self.position + size as u64;
-    // }
+}
+
+type Result = std::result::Result<u8, BitReadError>;
+
+#[derive(Debug, Clone, PartialEq)]
+struct BitReadError;
+
+impl fmt::Display for BitReadError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Unable to read bits")
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::result;
+
     use super::*;
 
     #[test]
-    fn read_bits() {
+    fn read_bits_from_single_item() {
         let input: &[u8] = &[10];
-        let bitreader = Bitreader::new(input);
+        let mut bitreader = Bitreader::new(input);
 
-        let result = bitreader.read_bits(8);
+        let result = bitreader.read_bits().unwrap();
         assert_eq!(result, 10)
+    }
+
+    #[test]
+    fn read_bits_from_multiple_items() {
+        let input: &[u8] = &[10, 20, 30];
+        let mut bitreader = Bitreader::new(input);
+
+        let result = bitreader.read_bits().unwrap();
+        println!("{}", result);
+        assert_eq!(result, 10);
+
+        let result = bitreader.read_bits().unwrap();
+        println!("{}", result);
+        assert_eq!(result, 20);
+
+        let result = bitreader.read_bits().unwrap();
+        println!("{}", result);
+        assert_eq!(result, 30)
+    }
+
+    #[test]
+    fn read_bits_err_if_size_too_large() {
+        let input: &[u8] = &[10];
+        let mut bitreader = Bitreader::new(input);
+
+        let result = bitreader.read_bits().err().unwrap();
+        assert_eq!(result, BitReadError)
     }
 }
